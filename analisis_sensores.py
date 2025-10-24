89a474195e217126540eccea7309763a8c5dbea1
3staurolita <kgonzaleza@unal.edu.co>
2025-10-23 19:14:49 -0500

    conversor_unidades

---
+++ analisis_sensores.py
@@ -0,0 +1,5 @@
+temperatura_celcius = 35.5
+temperatura_fahrenheit = temperatura_celcius*(9/5) + 32
+temperatura_kelvin = temperatura_celcius + 273.15 
+print(f"La temperatura en celsius es {temperatura_celcius}, en fahrenheit es {temperatura_fahrenheit} y en Kelvin es {temperatura_kelvin}")
+
---
+++ tallerpython1.py
@@ -0,0 +1,4 @@
+lado1 = 5 
+lado2 = 5
+area_cuadrado = lado1*lado2
+print(f"el area es: {area_cuadrado}")
